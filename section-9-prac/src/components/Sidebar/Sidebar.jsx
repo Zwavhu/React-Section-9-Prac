e90546b06fda69ab458e63378c4826b1f5d13fc8
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 13:32:10 +0200

    Fix sidebar size

--- section-9-prac/src/components/Sidebar/Sidebar.jsx
+++ section-9-prac/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 function Sidebar() {
     return (
-        <aside className="p-8 h-screen w-1/3 bg-blue-900 mt-12 rounded-tr-2xl">
-            <div className="w-1/3 flex flex-col">
+        <aside className="w-1/3 p-8 h-screen bg-blue-900 mt-12 rounded-tr-2xl">
+            <div className=" flex flex-col">
                 <h2 className="text-2xl font-bold text-white uppercase">Your Projects</h2>
                 <div className="py-8">
                     <button className="p-3 bg-blue-200 rounded text-gray-500 hover:text-gray-700 hover:bg-blue-300">+ Add Project</button>
