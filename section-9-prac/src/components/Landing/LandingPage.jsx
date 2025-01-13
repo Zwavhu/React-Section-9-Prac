5d5f8bd27d3a79ab2ab943f69f5cff5756df6a58
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 14:06:26 +0200

    Refactor button and add a component for the button

--- section-9-prac/src/components/Landing/LandingPage.jsx
+++ section-9-prac/src/components/Landing/LandingPage.jsx
@@ -1,13 +1,14 @@
-function LandingPage() {
+import CreateProjectButton from "../CreateProjectButton/CreateProjectButton.jsx";
+
+function LandingPage({handleCreateClick,showCreatePage}) {
+    const type = 'landing';
 
     return (
         <div className="w-[70%] flex flex-col items-center mt-32 gap-5">
             <img className="object-contain w-16 h-16" src="logo.png" alt="logo"/>
             <h3 className=" text-2xl text-gray-600 font-bold"> No Projects Selected</h3>
             <p className="text-gray-500"> Select a project or get started with a new one</p>
-            <div className="p-3 bg-blue-500 rounded mt-4 hover:text-gray-500 hover:bg-blue-300">
-                <button className="text-gray-800 ">Create new project</button>
-            </div>
+            <CreateProjectButton handleCreateClick={handleCreateClick} showCreatePage={showCreatePage} buttonType={type}></CreateProjectButton>
         </div>
     )
 }
--- section-9-prac/src/components/Sidebar/Sidebar.jsx
+++ section-9-prac/src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,11 @@
-function Sidebar() {
+import CreateProjectButton from "../CreateProjectButton/CreateProjectButton.jsx";
+
+function Sidebar({handleCreateClick, showCreatePage}) {
     return (
         <aside className="w-1/3 p-8 h-screen bg-blue-900 mt-12 rounded-tr-2xl">
             <div className=" flex flex-col">
                 <h2 className="text-2xl font-bold text-white uppercase">Your Projects</h2>
-                <div className="py-8">
-                    <button className="p-3 bg-blue-200 rounded text-gray-500 hover:text-gray-700 hover:bg-blue-300">+ Add Project</button>
-                </div>
+                <CreateProjectButton showCreatePage={showCreatePage} handleCreateClick={handleCreateClick} buttonType='sidebar'></CreateProjectButton>
             </div>
         </aside>
     )
