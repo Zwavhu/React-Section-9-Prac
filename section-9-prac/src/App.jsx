33037b4d550cd40aba8658723d807040707df2d2
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 14:07:11 +0200

    Add button state to the App component so that the state is handled there

--- section-9-prac/src/App.jsx
+++ section-9-prac/src/App.jsx
@@ -1,14 +1,21 @@
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import { createPortal } from 'react-dom'
 import LandingPage from "./components/Landing/LandingPage.jsx";
+import {useState} from "react";
 
 function App() {
+    const [showCreatePage, setShowCreatePage] = useState(false);
+    console.log('Show Create---',showCreatePage);
+
+    function handleCreateClick() {
+        setShowCreatePage(true);
+    }
+
   return (
     <>
         <div className="flex">
-            {/*{createPortal(<Sidebar/>, document.body)}*/}
-            <Sidebar />
-            <LandingPage />
+            <Sidebar handleCreateClick={handleCreateClick} showCreatePage={showCreatePage} />
+            <LandingPage handleCreateClick={handleCreateClick} showCreatePage={showCreatePage} />
         </div>
 
     </>
