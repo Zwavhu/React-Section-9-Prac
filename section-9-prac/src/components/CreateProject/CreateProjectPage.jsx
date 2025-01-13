d1f9e7c169c8e613e3e640f8a2cbded209f0cf1f
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 14:41:56 +0200

    Button cleanup and adding handle functions

--- section-9-prac/src/components/CreateProject/CreateProjectPage.jsx
+++ section-9-prac/src/components/CreateProject/CreateProjectPage.jsx
@@ -1,23 +1,27 @@
 import CreateProjectInput from "../CreateProjectInput/CreateProjectInput.jsx";
 
-function CreateProjectPage() {
+function CreateProjectPage({}) {
+
+
+    function handleSave(){
+
+    }
+
+    function handleCancel(){
+
+    }
 
     return (
         <>
             <div className="w-[70%] flex flex-col p-16 mt-32 gap-5">
                 <div className="flex gap-4 items-center justify-end">
-                    <button className="bg-white text-gray-700 hover:text-gray-500">Cancel</button>
-                    <button className="py-1.5 px-3.5 text-white bg-blue-500 rounded">Save</button>
+                    <button onClick={handleSave} className="bg-white text-gray-700 hover:text-gray-500">Cancel</button>
+                    <button onClick={handleCancel} className="py-1.5 px-3.5 text-white bg-blue-500 rounded hover:text-gray-500 hover:bg-blue-400">Save</button>
                 </div>
 
                 <CreateProjectInput label="Title" inputType="text" />
                 <CreateProjectInput label="Description" inputType="textarea" />
                 <CreateProjectInput label="Date" inputType="date" />
-
-                {/*<div className="flex flex-col">*/}
-                {/*    <label htmlFor="" className=" text-gray-700 font-bold uppercase">Title</label>*/}
-                {/*    <input className="bg-gray-200" type="text"/>*/}
-                {/*</div>*/}
             </div>
 
         </>
