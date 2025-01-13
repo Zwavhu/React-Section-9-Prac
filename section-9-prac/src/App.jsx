4fa9bd552090e752c60ccf170316cffd56ce9027
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 16:30:15 +0200

    Add handle save and cancel button functions

--- section-9-prac/src/App.jsx
+++ section-9-prac/src/App.jsx
@@ -5,15 +5,28 @@ import CreateProjectPage from "./components/CreateProject/CreateProjectPage.jsx"
 
 function App() {
     const [showCreatePage, setShowCreatePage] = useState(false);
+    const [saveProject, setSaveProject] = useState(false);
+    const [cancelSaveProject, setCancelSaveProject] = useState(false);
+    console.log('creee',saveProject);
+    console.log('creee',cancelSaveProject);
+
     function handleCreateClick() {
         setShowCreatePage(true);
+    }
+
+    function handleSave(){
+        setSaveProject(true);
+    }
+
+    function handleCancel(){
+        setCancelSaveProject(true);
     }
 
   return (
     <>
         <div className="flex">
             <Sidebar handleCreateClick={handleCreateClick} />
-            {showCreatePage ? <CreateProjectPage /> : <LandingPage handleCreateClick={handleCreateClick} />}
+            {showCreatePage ? <CreateProjectPage handleSave={handleSave} handleCancel={handleCancel} /> : !showCreatePage || cancelSaveProject ? <LandingPage handleCreateClick={handleCreateClick} /> : null}
         </div>
 
     </>
--- section-9-prac/src/components/CreateProject/CreateProjectPage.jsx
+++ section-9-prac/src/components/CreateProject/CreateProjectPage.jsx
@@ -1,22 +1,14 @@
 import CreateProjectInput from "../CreateProjectInput/CreateProjectInput.jsx";
 
-function CreateProjectPage({}) {
+function CreateProjectPage({handleSave, handleCancel}) {
 
-
-    function handleSave(){
-
-    }
-
-    function handleCancel(){
-
-    }
 
     return (
         <>
             <div className="w-[70%] flex flex-col p-16 mt-32 gap-5">
                 <div className="flex gap-4 items-center justify-end">
-                    <button onClick={handleSave} className="bg-white text-gray-700 hover:text-gray-500">Cancel</button>
-                    <button onClick={handleCancel} className="py-1.5 px-3.5 text-white bg-blue-500 rounded hover:text-gray-500 hover:bg-blue-400">Save</button>
+                    <button onClick={handleCancel} className="bg-white text-gray-700 hover:text-gray-500">Cancel</button>
+                    <button onClick={handleSave} className="py-1.5 px-3.5 text-white bg-blue-500 rounded hover:text-gray-500 hover:bg-blue-400">Save</button>
                 </div>
 
                 <CreateProjectInput label="Title" inputType="text" />
