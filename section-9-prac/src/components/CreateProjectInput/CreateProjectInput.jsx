a0d46e3686b36c92c152c8c4f501817de9f47707
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 16:30:35 +0200

    Styles cleanup

--- section-9-prac/src/components/CreateProjectInput/CreateProjectInput.jsx
+++ section-9-prac/src/components/CreateProjectInput/CreateProjectInput.jsx
@@ -3,11 +3,11 @@ function CreateProjectInput({inputType, label}) {
 
         <div className="flex flex-col">
             <label htmlFor="" className=" text-gray-700 font-bold uppercase">{label}</label>
-            <textarea className="bg-gray-200"></textarea>
+            <textarea className="bg-gray-200 focus:outline-none focus:border-gray-700 border-b-2" required></textarea>
         </div> :
         <div className="flex flex-col">
-            <label htmlFor="" className=" text-gray-700 font-bold uppercase">{label}</label>
-            <input className="bg-gray-200" type={inputType}/>
+            <label className="text-gray-700 font-bold uppercase">{label}</label>
+            <input className="bg-gray-200 focus:outline-none focus:border-gray-700 border-b-2" type={inputType} required/>
         </div>
     return (
         <>
