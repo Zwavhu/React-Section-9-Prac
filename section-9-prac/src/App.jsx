4327ff8a9647a8f148e8ca5fed458c35e8b90409
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 13:06:55 +0200

    Inital commit

---
+++ section-9-prac/src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+
+function App() {
+  return (
+    <>
+        <Sidebar />
+        <h1 className=" w-[70%] my-8 text-center font-bold">Hello World</h1>
+    </>
+  );
+}
+
+export default App;
