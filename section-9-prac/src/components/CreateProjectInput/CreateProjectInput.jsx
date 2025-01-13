e31f17bd2fa5da05f0b05f54d994ca3a140e0045
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 17:15:31 +0200

    Getting element props

--- section-9-prac/src/components/CreateProjectInput/CreateProjectInput.jsx
+++ section-9-prac/src/components/CreateProjectInput/CreateProjectInput.jsx
@@ -1,13 +1,13 @@
-function CreateProjectInput({inputType, label}) {
+function CreateProjectInput({inputType, label, ...props}) {
     const input = inputType === 'textarea' ?
 
         <div className="flex flex-col">
             <label htmlFor="" className=" text-gray-700 font-bold uppercase">{label}</label>
-            <textarea className="bg-gray-200 focus:outline-none focus:border-gray-700 border-b-2" required></textarea>
+            <textarea {...props} className="bg-gray-200 focus:outline-none focus:border-gray-700 border-b-2" required></textarea>
         </div> :
         <div className="flex flex-col">
             <label className="text-gray-700 font-bold uppercase">{label}</label>
-            <input className="bg-gray-200 focus:outline-none focus:border-gray-700 border-b-2" type={inputType} required/>
+            <input {...props} className="bg-gray-200 focus:outline-none focus:border-gray-700 border-b-2" type={inputType} required/>
         </div>
     return (
         <>
