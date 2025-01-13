6fddd8ae83feaeec62916fd94298a5a56cc37346
Zwavhu <zwavhudi@entrostat.com>
2025-01-13 13:12:17 +0200

    Making sidebar and content lay side by side horizontally

--- section-9-prac/src/App.jsx
+++ section-9-prac/src/App.jsx
@@ -3,8 +3,11 @@ import Sidebar from "./components/Sidebar/Sidebar.jsx";
 function App() {
   return (
     <>
-        <Sidebar />
-        <h1 className=" w-[70%] my-8 text-center font-bold">Hello World</h1>
+        <div className="flex">
+            <Sidebar />
+            <h1 className="w-[70%] my-8 text-center font-bold">Hello World</h1>
+        </div>
+
     </>
   );
 }
